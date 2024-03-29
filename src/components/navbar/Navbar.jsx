cb7dcb585bf7c964fbae64d83245244bda938061
Pratham Matkar <prathammatkar2506@gmail.com>
2024-03-29 14:28:13 +0530

    navbar testing 2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,41 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import Logo from "../../assets/Logo.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeNavbar = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="main-navdiv sticky">
-      <Link to='/'>
-      <img src={Logo} alt="" className="logo" />
+    <div className={`main-navdiv sticky ${isOpen ? "open" : ""}`}>
+      <Link to='/' onClick={closeNavbar}>
+        <img src={Logo} alt="" className="logo" />
       </Link>
-      <input type="checkbox" name="" id="nav-toggler" className="fas fa-bars" />
+      <input
+        type="checkbox"
+        name=""
+        id="nav-toggler"
+        className="fas fa-bars"
+        checked={isOpen}
+        onChange={handleToggle}
+      />
       <nav className="navbar">
         <a data-text="home">
-          <Link to='/'>Home</Link>          
+          <Link to='/' onClick={closeNavbar}>Home</Link>
         </a>
         <a href="#" data-text="se03">
-        <Link to='/SE03'>se03</Link>
+          <Link to='/SE03' onClick={closeNavbar}>se03</Link>
         </a>
         <a href="#" data-text="se03 max">
-        <Link to='/SE03MAX'>se03 max</Link>
+        <Link to='/SE03MAX' onClick={closeNavbar}>se03 max</Link>
         </a>
         <a href="#" data-text="se03 lite">
-        <Link to='/SE03LITE'>se03 lite</Link>
+        <Link to='/SE03LITE' onClick={closeNavbar}>se03 lite</Link>
         </a>
         <a href="#" data-text="compare">
-        <Link to='/'>compare</Link>
+        <Link to='/' onClick={closeNavbar}>compare</Link>
         </a>
         <a href="#" data-text="about">
-        <Link to='/About'>about</Link> 
+        <Link to='/About' onClick={closeNavbar}>about</Link> 
         </a>
         <a href="#" data-text="contact">
-        <Link to='/Contact'>contact</Link>
+        <Link to='/Contact' onClick={closeNavbar}>contact</Link>
         </a>
         <div className="background-image"></div>
         <div className="social">
-          <a href="">
+        <a href="">
             <i class="fa-brands fa-instagram"></i>
           </a>
           <a href="">
