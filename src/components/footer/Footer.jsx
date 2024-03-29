5225156db0dd6b8a6c20896eb161ca11ca6e2f5a
Pratham Matkar <prathammatkar2506@gmail.com>
2024-03-29 15:08:10 +0530

    scrolls fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,47 +1,47 @@
-import React from 'react'
-import './Footer.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  const handleClick = () => {
+    window.scrollTo(0, 0); // Scrolls to the top of the page
+  };
+
   return (
     <div>
       <div className="ft-maindiv">
         <div className="ft-paddiv">
-        <h2>Let's connect and collaborate</h2>
-        <div className="ft-links">
-          <div className="ft-mails">
-            {/* <small>Skype Us</small> */}
-            {/* <p><a href="">sales.techcompose</a></p> */}
+          <h2>Let's connect and collaborate</h2>
+          <div className="ft-links">
+            <div className="ft-mails">
+              <small>Email</small>
+              <p><a href="">info@rhyno.in</a></p>
 
-            <small>Email</small>
-            <p><a href="">info@rhyno.in</a></p>
-
-            <small>Call on</small>
-            <p><a href="">+91 9023987528</a></p>
+              <small>Call on</small>
+              <p><a href="">+91 9023987528</a></p>
+            </div>
+            <div className="ft-add">
+              <small>Visit us</small>
+              <p>Rhyno Wheels Private limited, Near UG hostel gate #2, Behind PDEU, Raisan, Gandhinagar, Gujarat, India.</p>
+            </div>
+            <div className="ft-lets">
+              <small>Lets connect</small>
+              <p><a href="mailto:info@rhyno.in" onClick={handleClick}>Drop a Message <i className="fas fa-chevron-right"></i></a></p>
+            </div>
           </div>
-          <div className="ft-add">
-            <small>Visit us</small>
-            <p>Rhyno Wheels Private limited, Near UG hostel gate #2, Behind PDEU, Raisan, Gandhinagar, Gujarat, India.</p>
-          </div>
-          <div className="ft-lets">
-            <small>Lets connect</small>
-            <p><a href="mailto:info@rhyno.in">Drop a Message <i class="fas fa-chevron-right"></i></a></p>
-          </div>
-        </div>
 
           <small>Quick Links</small>
-        <div className="ft-quick">
-            <Link to='/Privacy'><li>Privacy</li></Link>
-            <Link to='/Contact'><li>Contact Us</li></Link>
+          <div className="ft-quick">
+            <Link to='/Privacy' onClick={handleClick}><li>Privacy</li></Link>
+            <Link to='/Contact' onClick={handleClick}><li>Contact Us</li></Link>
             <li>Careers</li>
-            <Link to='/'><li>Rentals</li></Link>
-            {/* <li>Home</li> */}
-        </div>
-        <p>Copyright &copy; 2024 Rhyno Wheels Private Limited. All Rights Reserved.</p>
+            <Link to='/' onClick={handleClick}><li>Rentals</li></Link>
+          </div>
+          <p>Copyright &copy; 2024 Rhyno Wheels Private Limited. All Rights Reserved.</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
